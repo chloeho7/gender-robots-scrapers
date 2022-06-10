d53db086efb67b2ce924504d14de32f0c84f28d7
Chloe Ho <chloeho@Chloes-MacBook-Pro-2.local>
2022-06-10 17:22:08 -0400

    finalized fsr-scraper w/o print statements and correctfile name

--- fsr_scraper.py
+++ fsr_scraper.py
@@ -9,9 +9,6 @@ from bs4 import BeautifulSoup
 
 import json
 
-def find_author(tag):
-    return not tag.has_attr('class') and tag.has_attr('span')
-
 URL = "https://link.springer.com/book/10.1007/978-981-15-9460-1?page={}&oscar-books=true#toc"
 pagenum = 1
 
@@ -19,7 +16,7 @@ api_link = "https://api.semanticscholar.org/graph/v1/paper/{}?fields=citationCou
 
 api_author_link="https://api.semanticscholar.org/graph/v1/paper/{}/authors?fields=url"
 
-outfile = '0910test.csv'
+outfile = 'fsr_2019.csv'
 
 with open(outfile, 'w') as f:
 
@@ -28,16 +25,13 @@ with open(outfile, 'w') as f:
     while pagenum <=2:
 
         #get page
-        print(URL.format(pagenum))
         page = requests.get(URL.format(pagenum))
         soup = BeautifulSoup(page.content, "html.parser")
 
         #get confrence
         conf = soup.find('div',class_="c-book-evaluation-divider").contents[3].a.text
-        print(conf)
         #get year
         yr = soup.find_all('div',class_="c-book-evaluation-divider")[2].p.text[-48:-44]
-        print(yr)
         #get papers
         papers = soup.find_all('li',class_="c-card c-list-group__item c-card--flush u-pa-16")
 
@@ -49,12 +43,10 @@ with open(outfile, 'w') as f:
             f.write('"'+yr+'",')
             #write paper title
             title = paper.h3.a
-            print(title["href"])
             if pagenum == 1:
                 doi = title["href"][9:]
             else:
                 doi = title["href"][-28:]
-            print(doi)
             link = ("https://link.springer.com/chapter/"+doi)
             f.write('"'+title.text+'",')#changed to "" bc commas cause issues
             #write paper link
